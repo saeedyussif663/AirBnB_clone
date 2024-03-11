0eca7733522a35ae66de50765e7b586e449dbf65
saeedyussif663 <saeedyussif663@gmail.com>
2024-03-11 23:48:15 +0000

    files

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import uuid
+from uuid import uuid4
 from datetime import datetime
+import models
 
 """Base class
 - Public instance attribute
@@ -19,20 +20,40 @@ from datetime import datetime
 
 class BaseModel():
     """Base class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialization function"""
-        self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.now())
-        self.updated_at = str(datetime.now())
+        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key in ("updated_at", "created_at"):
+                    self.__dict__[key] = datetime.strptime(
+                        value, DATE_TIME_FORMAT)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """str method"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         """updates the object"""
-        self.updated_at = str(datetime.now())
+        self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
         """returns the attribute """
-        return self.__dict__
+        map_objects = {}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                map_objects[key] = value.strptime('%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                map_objects[key] = value
+        map_objects["__class__"] = self.__class__.__name__
+        return map_objects
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,50 @@
 #!/usr/bin/python3
 """
-This is a test case file
+Test suits for the base model
 """
+
+import os
+import re
+import json
+import uuid
 import unittest
+from time import sleep
+from datetime import datetime
+from models.base_model import BaseModel
 
-class TestBase(unittest.TestCase):
-    """This is the test case for the Base file"""
-    pass
 
+class TestBaseModel(unittest.TestCase):
+    """
+    Tests attributes of the base model
+    """
+
+    def setUp(self):
+        """
+        Classes needed for testing
+        """
+        pass
+
+    def test_basic(self):
+        """
+        Tests basic imputs for the BaseModel class
+        """
+        my_model = BaseModel()
+        my_model.name = "ALX"
+        my_model.number = 89
+        self.assertEqual([my_model.name, my_model.number],
+                         ["ALX", 89])
+
+    def test_datetime(self):
+        """
+        Tests for correct datetime format
+        """
+        pass
+    
+    def test_datetime(self):
+        """
+        Tests for correct datetime format
+        """
+        pass
 
 
 if __name__ == '__main__':
