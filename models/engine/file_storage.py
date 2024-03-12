bc514b642a53eec2a5c9edcfa924277caf81e7de
saeedyussif663 <saeedyussif663@gmail.com>
2024-03-12 11:32:38 +0000

    Revert "file storage"
    This reverts commit 4847835964bf37616474133323c5e1c15dda8eef.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,14 +5,8 @@ deserializes JSON types
 """
 
 import json
-from os.path import exists
 from models.base_model import BaseModel
 from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 
 class FileStorage:
@@ -22,15 +16,6 @@ class FileStorage:
 
     __file_path = "file.json"
     __objects = {}
-    __models = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Place": Place,
-            "Review": Review
-    }
 
     def all(self):
         """
@@ -38,7 +23,7 @@ class FileStorage:
         """
         return self.__objects
 
-    def new(self, obj):
+    def new(self, object):
         """sets in __objects the object with the key
         <object class name>.id
 
@@ -46,36 +31,27 @@ class FileStorage:
             object(obj): object to write
 
         """
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        self.__objects[object.__class__.__name__ + '.' + str(object)] = object
 
     def save(self):
         """
         serializes __objects to the JSON file
         (path: __file_path)
         """
-        serialised_objects = {}
-
-        for key, obj in self.__objects.items():
-            serialised_objects[key] = obj.to_dict()
-
-        with open(self.__file_path, "w", encoding="utf-8") as file:
-            json.dump(serialised_objects, file, indent=4)
+        with open(self.__file_path, 'w+') as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()
+                       }, f)
 
     def reload(self):
         """
         deserializes the JSON file to __objects, if the JSON
         file exists, otherwise nothing happens)
         """
-        if exists(self.__file_path):
-            with open(self.__file_path, "r", encoding="utf-8") as file:
-                instances = json.load(file)
-
-            for key, obj_dict in instances.items():
-                class_name, obj_id = key.split(".")
-
-                """ obj_instance = getattr(
-                __import__(class_name), class_name)(**obj_dict) """
-                obj_instance = self.__models[class_name](**obj_dict)
-
-                self.__objects[key] = obj_instance
+        try:
+            with open(self.__file_path, 'r') as f:
+                dict = json.loads(f.read())
+                for value in dict.values():
+                    cls = value["__class__"]
+                    self.new(eval(cls)(**value))
+        except Exception:
+            pass
